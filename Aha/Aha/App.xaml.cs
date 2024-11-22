7d1c2e878a9c5e4582d7c56fc44147e082703cf3
Alexander Lopez <115580820+Alex-J-Lopez@users.noreply.github.com>
2024-11-21 22:05:45 -0600

    Pushing Maui App

---
+++ Aha/Aha/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Aha
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Aha/Aha/ViewModels/AbstractViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aha.ViewModels
+{
+    public abstract class AbstractViewModel : ObservableObject, IQueryAttributable
+    {
+        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        {
+
+        }
+    }
+}
