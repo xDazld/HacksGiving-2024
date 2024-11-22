78ae6d45ce5a6723f453f16cf9d583dc2c132366
Patrick <pcr@doggo.dance>
2024-11-21 20:19:15 -0600

    Initial model output test

--- AhaAPI/main.py
+++ AhaAPI/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
-
+from gpt4all import GPT4All
 app = FastAPI()
-
+model = GPT4All("Llama-3.2-1B-Instruct-Q4_0.gguf")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    with model.chat_session():
+        return {"message": model.generate("Hello World")}
+
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    with model.chat_session():
+        return {"message": model.generate(f"Hello {name}")}
