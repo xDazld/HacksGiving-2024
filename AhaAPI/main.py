f8a55bcf229480b820248188aaf197ce46490402
Patrick <pcr@doggo.dance>
2024-11-21 20:44:24 -0600

    Streaming output

--- AhaAPI/main.py
+++ AhaAPI/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import StreamingResponse
 from gpt4all import GPT4All
+
 app = FastAPI()
 model = GPT4All("Llama-3.2-1B-Instruct-Q4_0.gguf")
+
 
 @app.get("/")
 async def root():
     with model.chat_session():
-        return {"message": model.generate("Hello World")}
-
+        return StreamingResponse(model.generate("Hello World", streaming=True))
 
 
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     with model.chat_session():
-        return {"message": model.generate(f"Hello {name}")}
+        return StreamingResponse(model.generate(f"Hello {name}", streaming=True))
