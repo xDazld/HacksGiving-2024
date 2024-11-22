4f32628a4974504fbb909dac4c42ec5bb02733d2
Patrick <pcr@doggo.dance>
2024-11-22 17:21:03 -0600

    Rudimentary history

--- AhaAPI/main.py
+++ AhaAPI/main.py
@@ -4,7 +4,7 @@ from gpt4all import GPT4All
 
 app = FastAPI()
 model = GPT4All("Llama-3.2-1B-Instruct-Q4_0.gguf")
-
+histories = dict()
 
 @app.get("/")
 async def root():
@@ -12,7 +12,16 @@ async def root():
         return StreamingResponse(model.generate("Hello World", streaming=True))
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
+@app.get("/chat/{name}/{prompt}")
+async def prompt_model(name: str, prompt: str):
     with model.chat_session():
-        return StreamingResponse(model.generate(f"Hello {name}", streaming=True))
+        if name in histories:
+            model._history = histories[name]
+        else:
+            histories[name] = model._history
+        return StreamingResponse(model.generate(f"{prompt}", streaming=True))
+
+
+@app.get("/history/{user_id}")
+async def get_history(user_id: str):
+    return histories[user_id]
