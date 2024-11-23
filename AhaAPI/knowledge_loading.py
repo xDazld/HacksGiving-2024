b9bacc899ed3aea069cc0a7aab519b1b07bbf937
Patrick <pcr@doggo.dance>
2024-11-23 17:37:49 -0600

    Go with a simple solution for exhibit context.

--- AhaAPI/knowledge_loading.py
+++ AhaAPI/knowledge_loading.py
@@ -1,15 +1,8 @@
 class ExhibitEmbeds:
     def __init__(self):
-        from gpt4all import Embed4All
         from pandas import read_excel
-
-        embedder = Embed4All()
-        exhibits = read_excel("../data/provided/Exhibit_Master_List.xlsx", skiprows=1)
-        self._embeddings = {}
-        for _, exhibit in exhibits.iterrows():
-            self._embeddings[exhibit["Experience Name"]] = embedder.embed(
-                exhibit.to_markdown()
-            )
+        self._embeddings = read_excel("../data/provided/Exhibit_Master_List.xlsx", skiprows=1,
+                                      index_col="Experience Name")
 
     def get_embedding(self, exhibit: str):
-        return self._embeddings[exhibit]
+        return self._embeddings.loc[exhibit]
