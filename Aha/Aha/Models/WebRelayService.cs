9e53d691bcd78d78546c36700b5573933c08dc00
nieberlen <noah@getoctane.com>
2024-11-23 09:04:13 -0600

    The actual last commit

--- Aha/Aha/Models/WebRelayService.cs
+++ Aha/Aha/Models/WebRelayService.cs
@@ -18,7 +18,7 @@ namespace Aha.Models;
     // Static field to hold the single instance of the client
     private static Socket? _instance;
 
-    public static readonly int portNumber = 8080;
+    public const int portNumber = 8080;
 
     IPEndPoint? ipEndPoint;
 
