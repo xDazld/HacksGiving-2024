e4a1b9c7db7ceb42e73462aa7ebfdc2e5a11fe91
Alexander Lopez <115580820+Alex-J-Lopez@users.noreply.github.com>
2024-11-22 22:22:12 -0600

    Implementing Initial User Quiz

---
+++ Aha/Aha/ViewModels/ContextQuizViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aha.ViewModels
+{
+    internal class ContextQuizViewModel : AbstractViewModel
+    {
+        private List<String> _languages = ["English","Espanol", "Francais"];
+        List<String> Languages { get { return _languages; } }
+        public String SelectedLanguage { get; set; }
+
+        public void SetLanguage()
+        {
+            System.Globalization.CultureInfo.CurrentUICulture = new System.Globalization.CultureInfo(SelectedLanguage);
+        }
+    }
+}
