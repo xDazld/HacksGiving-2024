32d4ca8a80593d89c4de948f947de2b78a4345a6
Patrick <pcr@doggo.dance>
2024-11-22 23:35:46 -0600

    Mark embeddings as protected

--- AhaAPI/knowledge_loading.py
+++ AhaAPI/knowledge_loading.py
@@ -4,9 +4,9 @@ class ExhibitEmbeds:
         from pandas import read_excel
         embedder = Embed4All()
         exhibits = read_excel("../data/provided/Exhibit_Master_List.xlsx", skiprows=1)
-        self.embeddings = dict()
+        self._embeddings = dict()
         for _, exhibit in exhibits.iterrows():
-            self.embeddings[exhibit["Experience Name"]] = embedder.embed(exhibit.to_markdown())
+            self._embeddings[exhibit["Experience Name"]] = embedder.embed(exhibit.to_markdown())
 
     def get_embedding(self, exhibit: str):
-        return self.embeddings[exhibit]
+        return self._embeddings[exhibit]
