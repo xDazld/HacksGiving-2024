369a15f0a6f4e1c9cd7eab6669c4dda392d6e88d
Patrick <pcr@doggo.dance>
2024-11-22 22:42:38 -0600

    Embedding class

---
+++ AhaAPI/knowledge_loading.py
@@ -0,0 +1,12 @@
+class ExhibitEmbeds:
+    def __init__(self):
+        from gpt4all import Embed4All
+        from pandas import read_excel
+        embedder = Embed4All()
+        exhibits = read_excel("../data/provided/Exhibit_Master_List.xlsx", skiprows=1)
+        self.embeddings = dict()
+        for _, exhibit in exhibits.iterrows():
+            self.embeddings[exhibit["Experience Name"]] = embedder.embed(exhibit.to_markdown())
+
+    def get_embedding(self, exhibit: str):
+        return self.embeddings[exhibit]
