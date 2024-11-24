772afc0b602c2acefaa53072419b8db3c8a93b56
Patrick R <32212089+xDazld@users.noreply.github.com>
2024-11-24 13:00:43 -0600

    Docs (#13)

--- AhaAPI/knowledge_loading.py
+++ AhaAPI/knowledge_loading.py
@@ -1,5 +1,8 @@
 class ExhibitEmbeds:
     def __init__(self):
+        """
+        Load the knowledge from the provided Excel file.
+        """
         from pandas import read_excel
 
         self._embeddings = read_excel(
@@ -8,10 +11,19 @@ class ExhibitEmbeds:
             index_col="Experience Name",
         )
 
-    def get_embedding(self, exhibit: str):
+    def get_embedding(self, exhibit: str) -> str:
+        """
+        Get the text for the given exhibit.
+        :param exhibit: the exhibit to get the text for
+        :return: the text for the exhibit
+        """
         return self._embeddings.loc[exhibit].to_markdown()
 
     def get_topics(self):
+        """
+        Get all the topics in the exhibit data.
+        :return: the list of topics
+        """
         topics_series = self._embeddings["Topics"].dropna()
         all_topics = set()
         for topics in topics_series:
--- AhaAPI/user.py
+++ AhaAPI/user.py
@@ -9,8 +9,15 @@ os.makedirs(USER_DB_PATH, exist_ok=True)
 
 
 class User:
+    """
+    User class to store user data
+    """
 
     def __init__(self, id: int):
+        """
+        Initializes the User class
+        :param id: the user id
+        """
         if os.path.exists(f"{USER_DB_PATH}{id}.json"):
             with open(f"{USER_DB_PATH}{id}.json", "r") as json_file:
                 self.user_data = json.load(json_file)
@@ -23,20 +30,42 @@ class User:
                 "history": [],
             }
 
-    def save(self):
+    def save(self) -> None:
+        """
+        Saves the user data
+        :return:
+        """
         with open(f"{USER_DB_PATH}{self.user_data['id']}.json", "w") as json_file:
             json.dump(self.user_data, json_file)
 
     def __del__(self):
+        """
+        Saves the user data when the object is deleted
+        :return:
+        """
         self.save()
 
     def get_history(self) -> list[MessageType]:
+        """
+        Gets the user history
+        :return: the user history
+        """
         return self.user_data["history"]
 
-    def set_history(self, history: list[MessageType]):
+    def set_history(self, history: list[MessageType]) -> None:
+        """
+        Sets the user history
+        :param history: the user history
+        :return:
+        """
         self.user_data["history"] = history
 
     def new_chat(self, data: dict) -> str:
+        """
+        Starts a new chat
+        :param data: the data for the chat
+        :return: the initial prompt
+        """
         context = data["userContext"]
         self.user_data["age"] = context["age"]
         self.user_data["language"] = context["language"]
@@ -45,14 +74,31 @@ class User:
         return "Tell me about this exhibit."
 
     def chat_message(self, data: dict) -> str:
+        """
+        Processes a chat message
+        :param data: the chat message data
+        :return: the prompt
+        """
         self.user_data["currentExhibit"] = data["currentExhibit"]
         return data["prompt"]
 
     def get_age(self) -> int:
+        """
+        Gets the user age
+        :return: the user
+        """
         return self.user_data["age"]
 
     def get_language(self) -> str:
+        """
+        Gets the user language
+        :return: the user language
+        """
         return self.user_data["language"]
 
     def get_exhibit(self) -> str:
+        """
+        Gets the current exhibit
+        :return: the current exhibit
+        """
         return self.user_data["currentExhibit"]
