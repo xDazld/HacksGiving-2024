846a36a9f084c67f34ac7d3d19f5e9ae369848c7
Patrick R <32212089+xDazld@users.noreply.github.com>
2024-11-23 21:02:20 -0600

    Feature/initial api (#4)
    * Initial model output test
    
    * Streaming output
    
    * OpenAPI Skeleton
    
    * Better OpenAPI Skeleton
    
    * Include hello world example in OpenAPI
    
    * Rudimentary history
    
    * Users
    
    * Embedding class
    
    * Data gitignore
    
    * Add websockets
    
    * Mark embeddings as protected
    
    * Return model response via sockets
    
    * style: format code with Black, ClangFormat, dotnet-format and isort
    
    This commit fixes the style issues introduced in 729324b according to the output
    from Black, ClangFormat, dotnet-format and isort.
    
    Details: https://github.com/xDazld/HacksGiving-2024/pull/4
    
    * Serialize tokens to JSON
    
    * Fix async error
    
    * Fix Deepsource found issues
    
    * style: format code with Black, ClangFormat, dotnet-format and isort
    
    This commit fixes the style issues introduced in 47b83ed according to the output
    from Black, ClangFormat, dotnet-format and isort.
    
    Details: https://github.com/xDazld/HacksGiving-2024/pull/4
    
    * style: format code with Black, ClangFormat, dotnet-format and isort
    
    This commit fixes the style issues introduced in b186fe2 according to the output
    from Black, ClangFormat, dotnet-format and isort.
    
    Details: https://github.com/xDazld/HacksGiving-2024/pull/4
    
    * Ignore config files
    
    * Fix Deepsource found issues x2
    
    * refactor: autofix issues in 1 file
    
    Resolved issues in AhaAPI/user.py with DeepSource Autofix
    
    * ci: update .deepsource.toml
    
    * Go with a simple solution for exhibit context.
    
    * Go with a simple solution for exhibit context. (and remember to make it a Markdown string)
    
    * Add testing document
    
    * Topics api, system prompt and exhibit info in prompts.
    
    * add websockets to poetry
    
    * Fix output
    
    * Attempt concurrency, more context for model
    
    * Formatting via black
    
    * Run black
    
    * Add current exhibit and chat message to prompts
    
    * Send EOT character
    
    ---------
    
    Co-authored-by: TravisJ33304 <dev@travis-jankowski.com>
    Co-authored-by: deepsource-autofix[bot] <62050782+deepsource-autofix[bot]@users.noreply.github.com>
    Co-authored-by: deepsource-io[bot] <42547082+deepsource-io[bot]@users.noreply.github.com>

---
+++ AhaAPI/user.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+from gpt4all.gpt4all import MessageType
+
+USER_DB_PATH = ".config/hacksgiving-2024/users/"
+
+os.makedirs(USER_DB_PATH, exist_ok=True)
+
+
+class User:
+
+    def __init__(self, id: int):
+        if os.path.exists(f"{USER_DB_PATH}{id}.json"):
+            with open(f"{USER_DB_PATH}{id}.json", "r") as json_file:
+                self.user_data = json.load(json_file)
+        else:
+            self.user_data = {
+                "id": id,
+                "age": 10,
+                "language": "en",
+                "interests": [],
+                "history": [],
+            }
+
+    def save(self):
+        with open(f"{USER_DB_PATH}{self.user_data['id']}.json", "w") as json_file:
+            json.dump(self.user_data, json_file)
+
+    def __del__(self):
+        self.save()
+
+    def get_history(self) -> list[MessageType]:
+        return self.user_data["history"]
+
+    def set_history(self, history: list[MessageType]):
+        self.user_data["history"] = history
+
+    def new_chat(self, data: dict) -> str:
+        context = data["userContext"]
+        self.user_data["age"] = context["age"]
+        self.user_data["language"] = context["language"]
+        self.user_data["interests"] = context["interests"]
+        self.user_data["currentExhibit"] = data["currentExhibit"]
+        return "Tell me about this exhibit."
+
+    def chat_message(self, data: dict) -> str:
+        self.user_data["currentExhibit"] = data["currentExhibit"]
+        return data["prompt"]
+
+    def get_age(self) -> int:
+        return self.user_data["age"]
+
+    def get_language(self) -> str:
+        return self.user_data["language"]
+
+    def get_exhibit(self) -> str:
+        return self.user_data["currentExhibit"]
