b0a9608c3335500dee193b7a54f8e276dc15b0ee
Alexander Lopez <115580820+Alex-J-Lopez@users.noreply.github.com>
2024-11-24 00:41:54 -0600

    Copying LocationContext CSV to app directory

--- Aha/Aha/App.xaml.cs
+++ Aha/Aha/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using Aha.Models;
 using Aha.Views.InitialContextQuiz;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace Aha
 {
@@ -9,7 +11,7 @@ namespace Aha
         {
             InitializeComponent();
 
-            if(File.Exists(UserContextManager.getUserContext().FilePath))
+            if (File.Exists(UserContextManager.getUserContext().FilePath))
             {
                 MainPage = new AppShell();
             }
@@ -18,5 +20,30 @@ namespace Aha
                 MainPage = new QInitial();
             }
         }
+
+        protected override void OnStart()
+        {
+            CopyFileIfNotExistsAsync("LocationContexts.csv");
+        }
+
+        public static async Task CopyFileIfNotExistsAsync(string fileName)
+        {
+            var destinationPath = Path.Combine(FileSystem.AppDataDirectory, fileName);
+
+            // Check if the file already exists
+            if (File.Exists(destinationPath))
+            {
+                return;
+            }
+
+            // Get the stream from the embedded resource
+            using var stream = await FileSystem.OpenAppPackageFileAsync(fileName);
+            using var reader = new StreamReader(stream);
+            using var writer = new StreamWriter(destinationPath);
+
+            // Copy the file
+            await writer.WriteAsync(await reader.ReadToEndAsync());
+        }
     }
 }
+
